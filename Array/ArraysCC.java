8e612fe90f0dea5c299e1f5dd62696522cdd8757
Ankan Basu <ankanbasu10@gmail.com>
2025-07-13 13:25:22 +0530

    Done

--- Array/ArraysCC.java
+++ Array/ArraysCC.java
@@ -1,10 +1,38 @@
 package Array;
 public class ArraysCC {
+    
+    /*  public static void update(int marks[], int nonchangeble  ) {  //  // Calling by reference
+        nonchangeble = 10;
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = marks[i] + 1;
+        }
+    }
+
+        public static void main(String[] args) {
+        //int marks[] = new int[5];
+
+        int marks[] = {97, 98, 99};
+        int nonchangeble = 5;
+        update(marks, nonchangeble); 
+        System.out.println(nonchangeble);
+
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i] + " ");
+        }
+
+    } */
     public static void main(String[] args) {
+       
+    }
 
-        int marks[] = new int[5];
-
-        int marks1[] = {10, 20, 30, 40, 50};
-
+   
+    public static int linearSearch(int arr[], int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i; 
+            }
+        }
+        return -1; 
+    }
 }
-}
+ 
